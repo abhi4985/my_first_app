0b7c87f2b6f05e6f7dea99efdf58f73204b11dbd
abhishek <abhishekgtm74@gmail.com>
2021-09-20 14:45:55 +0530

    new update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.hello).setOnClickListener {
+            Toast.makeText(this, "click on hell0", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
